f4b3ceb69f302ffca9e6581c27f4a5566b4070b1
cash <cash@cashwu.com>
2020-08-27 11:31:14 +0800

    init

---
+++ testNewLifeCache/Controllers/WeatherForecastController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NewLife.Caching;
+
+namespace testNewLifeCache.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IEnumerable<ICache> _caches;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IEnumerable<ICache> caches)
+        {
+            _logger = logger;
+            _caches = caches;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var key = "cash-user";
+
+            _caches.FirstOrDefault(a => a.GetType().Name == CacheProvider.MemoryCache.ToString())
+                   .Set(key, new User { Name = "NewLife Memory", CreateTime = DateTime.Now }, 3600);
+            
+            _caches.FirstOrDefault(a => a.GetType().Name == CacheProvider.FullRedis.ToString())
+                   .Set(key, new User { Name = "NewLife Redis", CreateTime = DateTime.Now }, 3600);
+
+            foreach (var cache in _caches)
+            {
+                Console.WriteLine(cache.GetType().Name);
+                var user = cache.Get<User>(key);
+
+                Console.WriteLine(user);
+            }
+            
+            var rng = new Random();
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                             {
+                                 Date = DateTime.Now.AddDays(index),
+                                 TemperatureC = rng.Next(-20, 55),
+                                 Summary = Summaries[rng.Next(Summaries.Length)]
+                             })
+                             .ToArray();
+        }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+
+        public DateTime CreateTime { get; set; }
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(CreateTime)}: {CreateTime:O}";
+        }
+    }
+
+    enum CacheProvider : byte
+    {
+        MemoryCache = 1,
+        FullRedis = 2
+    }
+}
---
+++ testNewLifeCache/Startup.cs
@@ -0,0 +1,59 @@
+using Autofac;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NewLife.Caching;
+
+namespace testNewLifeCache
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            builder.Register(context =>
+            {
+                FullRedis.Register();
+
+                return new FullRedis("127.0.0.1:6379", "cash1234", 0);
+            }).As<ICache>().SingleInstance();
+            
+            builder.Register(context => Cache.Default)
+                   .As<ICache>().SingleInstance()
+                   .PreserveExistingDefaults();
+       }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
